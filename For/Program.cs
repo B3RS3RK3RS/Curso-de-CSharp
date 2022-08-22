1331d1710c238d6c01df51ad4d68c11e324a396e
JESUS GUSTAVO FERNANDEZ SAAVEDRA <gustavofernandezsaavedra@gmail.com>
2022-08-21 22:51:57 -0500

    For

---
+++ For/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friends = new string[7]{
+                "Arlandria","Jesus","Gustavo","Anali","Roxana","Frank","Pablo"
+            };
+
+            bool run = true;
+            for (int i = 0; i < friends.Length; i++)
+            {
+                Console.WriteLine(friends[i]);
+            }
+        }
+    }
+}
