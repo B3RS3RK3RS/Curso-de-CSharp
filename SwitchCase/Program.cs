8cd9a262850044d2b6b8254a912150bdaf64236f
JESUS GUSTAVO FERNANDEZ SAAVEDRA <gustavofernandezsaavedra@gmail.com>
2022-08-21 22:18:16 -0500

    SwitchCase
    Cambio de netcore a 5.

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int op = 8;
+            switch (op)
+            {
+                case 1:
+                    Console.WriteLine("Seleccionaste el 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Seleccionaste el 2");
+                    break;
+                case 3:
+                case 4:
+                    Console.WriteLine("Seleccionaste 3 o 4");
+                    break;
+                case < 0:
+                case > 100:
+                    Console.WriteLine("Fuera de rango");
+                    break;
+                case > 4 and < 10:
+                    Console.WriteLine("Seleccionaste entre 4 y 10");
+                    break;
+                default:
+                    Console.WriteLine("Invalido");
+                    break;
+            }
+        }
+    }
+}
