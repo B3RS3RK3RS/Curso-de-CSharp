81dd01c9d890be59713c54d36f6d464bbe9a634b
JESUS GUSTAVO FERNANDEZ SAAVEDRA <gustavofernandezsaavedra@gmail.com>
2022-08-21 21:36:09 -0500

    Sentencias If, Else y Else If

---
+++ IfElse/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool tienesHambre = true;
+            bool tienesDinero = true;
+
+            if (tienesHambre && tienesDinero && restauranAbierto("24 horass"))
+            {
+                Console.WriteLine("COME");
+            }
+            else
+            {
+                Console.WriteLine("NO COMAS");
+            }
+        }
+
+        static bool restauranAbierto(string nombre, int hour = 0)
+        {
+            if (nombre == "Cena Pepe" && hour > 8 && hour < 23)
+            {
+                return true;
+            }
+            else if(nombre == "24 horas")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
