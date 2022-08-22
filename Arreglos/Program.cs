f86023740e79fd300c611db157d5772b85ba0f6c
JESUS GUSTAVO FERNANDEZ SAAVEDRA <gustavofernandezsaavedra@gmail.com>
2022-08-21 22:32:02 -0500

    Arreglos

---
+++ Arreglos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friends = new string[7]{
+                "Jesus","Gustavo","Anali","Roxana","Frank","Pablo",null
+            };
+
+            friends[0] = "Arlandria Michell Nicolle de Jesús";
+
+            Console.WriteLine(friends[0]);
+            Console.WriteLine(friends[1]);
+            Console.WriteLine(friends[2]);
+            Console.WriteLine(friends[3]);
+            Console.WriteLine(friends[4]);
+            Console.WriteLine(friends[5]);
+            Console.WriteLine(friends[6]);
+
+            friends[6] = "Jose";
+            Console.WriteLine(friends[6]);
+
+            friends[7] = "Fuerita de rango";
+        }
+    }
+}
