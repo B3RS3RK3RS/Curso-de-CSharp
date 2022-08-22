dabe904f71e9ee964870d23ed1c20a23979c9204
JESUS GUSTAVO FERNANDEZ SAAVEDRA <gustavofernandezsaavedra@gmail.com>
2022-08-21 19:36:14 -0500

    Tipos de Datos

---
+++ Tipos de Datos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tipos_de_Datos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 12121;
+            double doubleNumber = 12.21;
+            char character = 'A';
+            string name = "Jesús Gustavo Fernández Saavedra";
+            bool isDead = false;
+            Console.WriteLine(isDead);
+        }
+    }
+}
